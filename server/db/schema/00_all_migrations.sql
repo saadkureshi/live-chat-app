cb9dc7f7139acde4d47ec296f6ed57a64a7a848f
Saad Kureshi <saq619@gmail.com>
2021-10-12 18:15:16 -0400

    db schema and seeds added

---
+++ server/db/schema/00_all_migrations.sql
@@ -0,0 +1,31 @@
+-- Connect to postgres database
+\c postgres;
+
+-- Drop database if it exists
+DROP DATABASE IF EXISTS "live_chat_app";
+
+-- Create database
+CREATE DATABASE live_chat_app;
+
+-- Connect to shopify_challenge database
+\c live_chat_app;
+
+-- Create users table
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Create images table
+-- CREATE TABLE images (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   image_url TEXT,
+--   private BOOLEAN,
+--   created_at TIMESTAMP DEFAULT NOW()
+-- );
---
+++ server/db/seeds/01_users_seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (first_name, last_name, username, email, password)
+VALUES 
+('Saad', 'Kureshi', 'saadkureshi', 'saad@kureshi.com', '$2b$10$tn2O7rPgZlO5gzv52ni8z.lvJVD4uKdIbOS/FZIYYEWRBNgOzGKBm'),
+('Tom', 'Cruise', 'tomcruise', 'tom@cruise.com', '$2b$10$tn2O7rPgZlO5gzv52ni8z.lvJVD4uKdIbOS/FZIYYEWRBNgOzGKBm'),
+('Kanye', 'West', 'kanyewest', 'kanye@west.com', '$2b$10$tn2O7rPgZlO5gzv52ni8z.lvJVD4uKdIbOS/FZIYYEWRBNgOzGKBm'),
+('Cristiano', 'Ronaldo', 'cristianoronaldo', 'cristiano@ronaldo.com', '$2b$10$tn2O7rPgZlO5gzv52ni8z.lvJVD4uKdIbOS/FZIYYEWRBNgOzGKBm')
