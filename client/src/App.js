6c77f1b09d8a136efd866d0cd5c13b5f88872e57
Saad Kureshi <saq619@gmail.com>
2021-10-12 21:15:21 -0400

    navbar and login form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Import stylesheets
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import components
+import NavigationBar from './components/NavigationBar';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+      <LoginForm />
     </div>
   );
 }
