c79e53217a198b4c31207028f25b4a4e70d2678b
Saad Kureshi <saq619@gmail.com>
2021-10-13 20:42:08 -0400

    navbar updated

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -6,25 +6,16 @@ function NavigationBar() {
     <div className="navigation-bar">
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">The Debate Room</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/">Features</Nav.Link>
+            <Nav.Link href="/">Pricing</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            <Nav.Link href="/">Register</Nav.Link>
+            <Nav.Link href="/">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
         </Container>
